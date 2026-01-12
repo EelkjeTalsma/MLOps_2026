c43b501e4457e7d36de15b3fe45cb7506dbc1dc6
Eelkje Talsma <eelkje.talsma@student.uva.nl>
2026-01-12 18:38:29 +0100

    vraag 7

--- src/ml_core/data/loader.py
+++ src/ml_core/data/loader.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from typing import Dict, Tuple
-
+import numpy as np
 from torch.utils.data import DataLoader
 from torchvision import transforms
-
+from torch.utils.data import WeightedRandomSampler
 from .pcam import PCAMDataset
 
 
@@ -14,7 +14,7 @@ def get_dataloaders(config: Dict) -> Tuple[DataLoader, DataLoader]:
     """
     data_cfg = config["data"]
     base_path = Path(data_cfg["data_path"])
-
+	
     # TODO: Define Transforms
     # train_transform = ...
     # val_transform = ...
@@ -26,5 +26,37 @@ def get_dataloaders(config: Dict) -> Tuple[DataLoader, DataLoader]:
     # TODO: Create DataLoaders
     # train_loader = ...
     # val_loader = ...
-    
-    raise NotImplementedError("Implement get_dataloaders")
+    train_transform = transforms.Compose([])
+    val_transform = transforms.Compose([])
+
+    train_x = base_path / "camelyonpatch_level_2_split_train_x.h5"
+    train_y = base_path / "camelyonpatch_level_2_split_train_y.h5"
+    val_x = base_path / "camelyonpatch_level_2_split_valid_x.h5"
+    val_y = base_path / "camelyonpatch_level_2_split_valid_y.h5"
+
+    train_dataset = PCAMDataset(x_path=str(train_x), y_path=str(train_y), transform=train_transform)
+    val_dataset = PCAMDataset(x_path=str(val_x), y_path=str(val_y), transform=val_transform)
+
+    train_labels = np.array([train_dataset[i][1].item() for i in range(len(train_dataset))])
+    class_counts = np.bincount(train_labels)
+    class_weights = 1.0 / class_counts
+    sample_weights = class_weights[train_labels]
+    train_sampler = WeightedRandomSampler(weights=sample_weights,num_samples=len(sample_weights),replacement=True)
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=data_cfg.get("batch_size", 32),
+        sampler=train_sampler,
+        num_workers=data_cfg.get("num_workers", 4),
+        pin_memory=False
+    )
+
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=data_cfg.get("batch_size", 32),
+        shuffle=False,
+        num_workers=data_cfg.get("num_workers", 4),
+        pin_memory=False
+    )
+
+    return train_loader, val_loader
--- src/ml_core/data/pcam.py
+++ src/ml_core/data/pcam.py
@@ -12,20 +12,33 @@ class PCAMDataset(Dataset):
     PatchCamelyon (PCAM) Dataset reader for H5 format.
     """
 
-    def __init__(self, x_path: str, y_path: str, transform: Optional[Callable] = None):
+    def __init__(self, x_path: str, y_path: str, transform: Optional[Callable] = None, filter_data: bool = False):
         self.x_path = Path(x_path)
         self.y_path = Path(y_path)
         self.transform = transform
+        self.filter_data = filter_data
 
         # TODO: Initialize dataset
         # 1. Check if files exist
         # 2. Open h5 files in read mode
-        pass
+        if not self.x_path.exists() or not self.y_path.exists():
+            raise FileNotFoundError
+        self.x_h5 = h5py.File(self.x_path, "r")
+        self.y_h5 = h5py.File(self.y_path, "r")
+        self.images = self.x_h5["x"]
+        self.labels = self.y_h5["y"]
+        if filter_data:
+            self.indices = [
+                i for i in range(len(self.labels))
+                if 0 < np.mean(self.images[i]) < 255
+            ]
+        else:
+            self.indices = list(range(len(self.labels)))
 
     def __len__(self) -> int:
         # TODO: Return length of dataset
         # The dataloader will know hence how many batches to create
-        return 0
+        return len(self.labels)
 
     def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
         # TODO: Implement data retrieval
@@ -33,5 +46,16 @@ class PCAMDataset(Dataset):
         # 2. Convert to uint8 (for PIL compatibility if using transforms)
         # 3. Apply transforms if they exist
         # 4. Return tensor image and label (as long)
-        
-        raise NotImplementedError("Implement __getitem__ in PCAMDataset")
+        idx = self.indices[idx]
+        img = self.images[idx]
+        label = self.labels[idx]
+        img = np.clip(img, 0, 255)
+        # img = np.clip(img / 255.0, 0.0, 1.0).astype(np.float32)
+        # img_tensor = torch.from_numpy(img).permute(2, 0, 1)
+        # label_tensor = torch.tensor(label, dtype=torch.long)
+        img = torch.tensor(img, dtype=torch.float32).permute(2, 0, 1)
+        label = torch.tensor(label, dtype=torch.long).squeeze()
+        if self.transform is not None:
+            # img_tensor = self.transform(img_tensor)
+            img = self.transform(img)
+        return img, label
